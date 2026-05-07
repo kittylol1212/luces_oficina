04918027fb27e57ba7e15d44426a58fe5fa98b8f
unknown <Administrator@WIN-TGPM9PASJVD.meltec.dom>
2026-05-07 15:59:33 -0500

    cambio de URL

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,7 @@
 // ==========================================
 // CONFIGURACIÓN GLOBAL
 // ==========================================
-const BASE_URL = "https://attach-mel-defined-ambient.trycloudflare.com"; 
+const BASE_URL = "https://wifi-honey-pictures-during.trycloudflare.com"; 
 
 // ==========================================
 // 1. FUNCIÓN MAESTRA: TODO EL EDIFICIO (PISO 0)
