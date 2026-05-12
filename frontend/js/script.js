d3a462ccce7e95e65d6fa2258fada937ad5016ba
unknown <Administrator@WIN-TGPM9PASJVD.meltec.dom>
2026-05-12 09:52:35 -0500

    cambio url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,7 +1,7 @@
 // ==========================================
 // CONFIGURACIÓN GLOBAL
 // ==========================================
-const BASE_URL = "https://resort-intended-kept-syntax.trycloudflare.com"; 
+const BASE_URL = "https://occurrence-apache-plots-detection.trycloudflare.com"; 
 
 // ==========================================
 // 1. FUNCIÓN MAESTRA: TODO EL EDIFICIO (PISO 0)
