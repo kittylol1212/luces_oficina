2ae46ddd68e8dc6987665b37e0a3f91d0ab9f446
unknown <Administrator@WIN-TGPM9PASJVD.meltec.dom>
2026-05-20 15:01:24 -0500

    cambio url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://rouge-day-helps-strengthening.trycloudflare.com"; // <-- ⚠️ Recuerda actualizar este link si reinicias Cloudflare
+const BASE_URL = "https://fish-slide-round-pack.trycloudflare.com"; // <-- ⚠️ Recuerda actualizar este link si reinicias Cloudflare
 
 // ==========================================
 // 1. FUNCIÓN MAESTRA: TODO EL EDIFICIO (PISO 0)
