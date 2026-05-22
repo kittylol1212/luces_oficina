5d332e0fa0fd9d01182c5303c1f33d5543b3f6c1
unknown <Administrator@WIN-TGPM9PASJVD.meltec.dom>
2026-05-22 09:45:54 -0500

    Actualizando archivos del proyecto

--- frontend/js/stats_logic.js
+++ frontend/js/stats_logic.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://recommendations-bugs-economic-diagnostic.trycloudflare.com"; 
+const BASE_URL = "https://attributes-hierarchy-hwy-agreements.trycloudflare.com"; 
 
 document.addEventListener("DOMContentLoaded", function () {
     const JORNADA_MAXIMA = 10.0;
