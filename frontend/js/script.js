786b0b709f69f28d2fe140ab03afca4681fe4e51
unknown <Administrator@WIN-TGPM9PASJVD.meltec.dom>
2026-06-12 09:34:00 -0500

    Aquí va tu mensaje

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://casual-mph-php-exposed.trycloudflare.com"; // <-- ⚠️ Recuerda actualizar este link si reinicias Cloudflare
+const BASE_URL = "https://uncle-doe-engaged-mazda.trycloudflare.com "; // <-- ⚠️ Recuerda actualizar este link si reinicias Cloudflare
 
 // ==========================================
 // 1. FUNCIÓN MAESTRA: TODO EL EDIFICIO (PISO 0)
--- frontend/js/stats_logic.js
+++ frontend/js/stats_logic.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://casual-mph-php-exposed.trycloudflare.com"; 
+const BASE_URL = "https://uncle-doe-engaged-mazda.trycloudflare.com"; 
 
 document.addEventListener("DOMContentLoaded", function () {
     const JORNADA_MAXIMA = 10.0;
